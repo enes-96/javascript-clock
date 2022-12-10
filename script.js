f3c47973db8dfb7a316d7cb09ea49ae5c7720617
Enes <seyit7enes@gmail.com>
2022-12-10 17:28:26 +0100

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+setInterval(getTime, 1000);
+
+const secondHand = document.getElementById("secondHand");
+const minuteHand = document.getElementById("minuteHand");
+const hourHand = document.getElementById("hourHand");
+
+function getTime() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
